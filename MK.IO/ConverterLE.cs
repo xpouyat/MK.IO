9ced27c4ded9e30cf2ba4733117c915d0502b430
Xavier Pouyat <xpouyat@microsoft.com>
2024-03-08 18:59:12 +0100

    Reduce precision of DateTime to solve issue https://github.com/xpouyat/MK.IO/issues/43

--- MK.IO/ConverterLE.cs
+++ MK.IO/ConverterLE.cs
@@ -17,8 +17,28 @@ namespace MK.IO
             //DateParseHandling = DateParseHandling.None,
             Converters =
             {
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+                new CustomDateTimeConverter()
+                //new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
             },
         };
+    }
+
+    // We limit the conversion of DateTime to the format "yyyy-MM-ddTHH:mm:ss.ffZ"
+    internal class CustomDateTimeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(DateTime)) || (objectType == typeof(DateTime?));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return DateTime.Parse(reader.Value.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((DateTime)value).ToString("yyyy-MM-ddTHH:mm:ss.ffZ"));
+        }
     }
 }
