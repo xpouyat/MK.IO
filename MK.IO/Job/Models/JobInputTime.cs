855000d1dce118d31d7950083daa4cdc7d819bce
Xavier Pouyat <xpouyat@microsoft.com>
2024-05-17 15:50:24 +0200

    Remove old polymorphic lib

--- MK.IO/Job/Models/JobInputTime.cs
+++ MK.IO/Job/Models/JobInputTime.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using JsonSubTypes;
 using System.Text.Json.Serialization;
 
 namespace MK.IO.Models
--- MK.IO/Job/Models/Track/TrackDiscriminator.cs
+++ MK.IO/Job/Models/Track/TrackDiscriminator.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using JsonSubTypes;
 using System.Text.Json.Serialization;
 
 namespace MK.IO.Models
